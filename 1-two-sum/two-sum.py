b2afec8dd9bb3d44b0682400d4e844a4e3e1af53
BradT615 <125227539+BradT615@users.noreply.github.com>
2023-12-28 21:53:16 -0500

    Time: 39 ms (75.24%) | Memory: 14.3 MB (36.67%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,10 +5,9 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        result = []
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    result.append(i)
-                    result.append(j)
-                    return result
+        numsMap = {}
+
+        for i, num in enumerate(nums):
+            if (target - num) in numsMap:
+                return [numsMap[target - num], i]
+            numsMap[num] = i
